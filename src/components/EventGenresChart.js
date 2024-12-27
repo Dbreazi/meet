0707ef0137b36c117c8c6e9224be69b20690dd56
Dbreazi <157401683+Dbreazi@users.noreply.github.com>
2024-12-27 21:18:17 +0100

    Task 4.10 - Pt6?
    Adjusted responsiveness by adding max and min widths to the app container.
    
    Added spacing between labels and pie chart.
    
    Made label colour match the related pie chart slices.

--- src/components/EventGenresChart.js
+++ src/components/EventGenresChart.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import {
   PieChart,
   Pie,
@@ -9,33 +9,40 @@ import {
 const EventGenresChart = ({ events }) => {
   const [data, setData] = useState([]);
 
-  const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'Angular'];
+  
+  const genres = useMemo(() => ['React', 'JavaScript', 'Node', 'jQuery', 'Angular'], []);
 
-  const getData = () => {
-    const data = genres.map((genre) => {
-      const filteredEvents = events.filter(event => event.summary.includes(genre));
-      return {
-        name: genre,
-        value: filteredEvents.length
-      };
-    });
-    return data;
-  };
-
+ 
   useEffect(() => {
-    setData(getData());
-  }, [events]);
+    const getData = () => {
+      const data = genres.map((genre) => {
+        const filteredEvents = events.filter(event => event.summary.includes(genre));
+        return {
+          name: genre,
+          value: filteredEvents.length
+        };
+      });
+      return data;
+    };
 
+    setData(getData());
+  }, [events, genres]); 
+
+  
   const renderCustomizedLabel = ({ cx, cy, midAngle, outerRadius, percent, index }) => {
     const RADIAN = Math.PI / 180;
-    const radius = outerRadius;
-    const x = cx + radius * Math.cos(-midAngle * RADIAN) * 1.07;
-    const y = cy + radius * Math.sin(-midAngle * RADIAN) * 1.07;
+    const radius = outerRadius + 20; // Add some extra space between the outer edge and the label
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+  
+    
+    const sliceColor = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#D0AFFF'][index % 5];
+  
     return percent ? (
       <text
         x={x}
         y={y}
-        fill="#8884d8"
+        fill={sliceColor} 
         textAnchor={x > cx ? 'start' : 'end'}
         dominantBaseline="central"
       >
@@ -43,24 +50,26 @@ const EventGenresChart = ({ events }) => {
       </text>
     ) : null;
   };
+  
 
   return (
-    <ResponsiveContainer width="100%" height={400}>
+    <ResponsiveContainer width="99%" height={400}>
       <PieChart>
-        <Pie
-          data={data}
-          dataKey="value"
-          nameKey="name"
-          fill="#8884d8"
-          labelLine={false}
-          label={renderCustomizedLabel}
-          outerRadius={130}
-        >
-          {data.map((entry, index) => (
-            <Cell key={`cell-${index}`} fill={['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#D0AFFF'][index % 5]} />
-          ))}
-        </Pie>
-      </PieChart>
+  <Pie
+    data={data}
+    dataKey="value"
+    nameKey="name"
+    fill="#8884d8"
+    labelLine={false} 
+    label={renderCustomizedLabel}  
+    outerRadius={120} 
+  >
+    {data.map((entry, index) => (
+      <Cell key={`cell-${index}`} fill={['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#D0AFFF'][index % 5]} />
+    ))}
+  </Pie>
+</PieChart>
+
     </ResponsiveContainer>
   );
 };
